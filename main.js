f0c644545af62dd4b58de4052346c76917e759a6
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-01-17 12:31:39 +0200

    hw-2

---
+++ main.js
@@ -0,0 +1,7 @@
+let number1 = +prompt('Введи перше число!');
+let number2 = +prompt('Введи друге число!');
+let number3 = +prompt('Введи третє число!');
+
+let number4 = (number1 + number2 + number3) / 3;
+
+alert(`Ваше середнє арифметичне дорівнює ${number4}`);
