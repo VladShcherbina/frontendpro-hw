3d64b1acc016b936289870d7444efb8afea0c54e
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-01-24 14:51:50 +0200

    hw-4

---
+++ main.js
@@ -0,0 +1,9 @@
+let number1 = +prompt('Введи перше число!');
+let number2 = +prompt('Введи друге число!');
+
+
+alert(`Ви ввели ${number1} та ${number2}
+${number1} + ${number2} = ${number1 + number2}
+${number1} - ${number2} = ${number1 - number2}
+${number1} * ${number2} = ${number1 * number2}
+${number1} / ${number2} = ${number1 / number2}`)
