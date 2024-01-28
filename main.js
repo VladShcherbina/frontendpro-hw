6f46e56cc6268f44bc12e4e6b0c8d555e5f28f54
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-01-28 15:48:21 +0200

    Hw-9

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 console.log(array);
-console.log(array.indexOf(4));
-array.splice(3 , 1);
+const removeElement = function(arr, element){
+   let index = arr.indexOf(element)
+   arr.splice(index, 1)
+}
+removeElement(array, 5)
 console.log(array);
