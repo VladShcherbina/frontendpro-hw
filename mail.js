4191e90b6f0bf94c393232a52e55b90c653db19b
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-01-31 16:51:49 +0200

    hw-11

--- mail.js
+++ mail.js
@@ -1,14 +1,22 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+const getRandom = (max, min) => Math.floor(
+    Math.random () * (max - min + 1) + min
+)
+
 function generateKey (length, characters) {
     let min = 0;
-    let max = characters.length
-    let result = Math.floor(
-        Math.random () * (max - min + 1) + min
-    )
-    console.log(result)
+    let max = characters.length -1
+    let key = ''
+    
+    for(let i = 0; i < length; i++ ){
+     let randomNumber = getRandom(max, min);
+     let randomCharacters = characters[randomNumber]
+     key += randomCharacters
+    }
+    return key
 }
 
-const key = generateKey (16,characters);
+const key = generateKey (3,characters);
 
 console.log(key)
