5ef8e9397dacb14cf46a5556c665688464041fa2
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-17 19:54:23 +0200

    hw-19

---
+++ main.js
@@ -0,0 +1,23 @@
+// let string1 = prompt('Введіть першу строку')
+// let string2 = prompt('Введіть другу строку')
+// let string3 = prompt('Введіть третю строку')
+
+// alert(`${string1} ${string2} ${string3}`)
+
+
+// function splitAndReverseNumber(number) {
+//     if (number < 10000 || number > 99999) {
+//         return console.log("Введене число не є п'ятизначним."); 
+//     }
+//     let digits = number.toString().split('').join(' ');
+//     console.log(digits);
+// }
+// splitAndReverseNumber(12345);
+
+let number = '12345';
+let newNumber = ' '
+for(let i = 0; i < number.length; i++) {
+    let result = Math.floor(number / 10000)
+    
+    console.log(result)
+}
