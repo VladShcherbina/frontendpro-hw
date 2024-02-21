49b2fe8f03a25bc7675dd68f8bb51a080196a928
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-21 15:24:01 +0200

    Hw-22

---
+++ main.js
@@ -0,0 +1,37 @@
+const smiles = [
+    {
+        smileIkon: '😀',
+        voteCount: '0'
+    },{
+        smileIkon: '😆',
+        voteCount: '0'
+    },{
+        smileIkon: '🙂',
+        voteCount: '0'
+    },{
+        smileIkon: '😉',
+        voteCount: '0'
+    },{
+        smileIkon: '🥰',
+        voteCount: '0'
+    },
+]
+const appDiv = document.querySelector('#app')
+
+const createDiv = () => document.createElement('div')
+
+smiles.forEach((smile) => {
+    const wrapper = createDiv()
+    const smileIkonDiv = createDiv()
+    const voteCountDiv = createDiv()
+
+    smileIkonDiv.textContent = `${smile.smileIkon}`
+    voteCountDiv.textContent = `${smile.voteCount}`
+
+    wrapper.appendChild(smileIkonDiv)
+    wrapper.appendChild(voteCountDiv)
+    appDiv.appendChild(wrapper)
+    smileIkonDiv.addEventListener('click', () => {
+        voteCountDiv.textContent++
+    })
+})
