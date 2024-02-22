99c118b8af82639349d7fe1aa0c448f4403f5242
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-22 13:57:12 +0200

    hw-22

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
+const SMILES_KEY = 'smiles'
+
 const smiles = [
     {
         smileIkon: '😀',
-        voteCount: '0'
+        voteCount: '0',
     },{
         smileIkon: '😆',
-        voteCount: '0'
+        voteCount: '0',
     },{
         smileIkon: '🙂',
-        voteCount: '0'
+        voteCount: '0',
     },{
         smileIkon: '😉',
-        voteCount: '0'
+        voteCount: '0',
     },{
         smileIkon: '🥰',
-        voteCount: '0'
+        voteCount: '0',
     },
 ]
+
 const appDiv = document.querySelector('#app')
 
 const createDiv = () => document.createElement('div')
 
-smiles.forEach((smile) => {
+const localSmile = JSON.parse(localStorage.getItem(SMILES_KEY)) || smiles
+
+localSmile.forEach((smile) => {
     const wrapper = createDiv()
     const smileIkonDiv = createDiv()
     const voteCountDiv = createDiv()
@@ -34,5 +39,8 @@ smiles.forEach((smile) => {
     appDiv.appendChild(wrapper)
     smileIkonDiv.addEventListener('click', () => {
         voteCountDiv.textContent++
+        smile.voteCount++
+
+        localStorage.setItem(SMILES_KEY, JSON.stringify(smiles))
     })
 })
