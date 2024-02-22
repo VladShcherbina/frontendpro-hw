3e22f9247b1f142c0f2c6f20df350a1b4a249147
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-22 13:29:00 +0200

    hw-23

---
+++ main.js
@@ -0,0 +1,33 @@
+const todo_wrapper = document.querySelector('#todo_list') 
+const add_todo = document.querySelector('#add_todo')
+const todo_input = document.querySelector('#todo_input') 
+const todo_list = JSON.parse(localStorage.getItem('todo_list')) ? 
+     JSON.parse(localStorage.getItem('todo_list')) : []
+updateTodoList()
+
+
+function updateTodoList() {
+    todo_wrapper.innerHTML = '';
+    localStorage.setItem('todo_list', JSON.stringify(todo_list))
+    
+    todo_list.forEach(todo => {
+        const temp_todo = document.createElement('div');
+        temp_todo.textContent = todo.title;
+        temp_todo.classList.add('todo');
+        
+        todo_wrapper.appendChild(temp_todo)
+    })
+}
+
+add_todo.addEventListener('click', () => {
+    if(todo_input.value) {
+        todo_list.push({
+            title: todo_input.value,
+            complete: false,
+        })
+    }
+
+    updateTodoList()
+})
+
+
