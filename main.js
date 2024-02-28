b31fc6d752635c5d2c42707c0028c0222cc1e08f
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-28 15:41:17 +0200

    hw-24

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-const table = document.querySelector('#myTable')
-let isTrue = true
-
-for(i = 0; i < 3; i++) {
-    let tr = document.createElement('tr');
-
-    for(j = 0; j < 3; j++) {
-        let td = document.createElement('td');
-        td.style.fontSize = '40px'
-        td.style.textAlign = 'center' 
-        td.style.border = '2px solid #000000'
-        td.style.width = '400px'
-        td.style.height = '300px'
-		tr.appendChild(td);
-        td.addEventListener('click', () => {
-            if(isTrue) {
-                td.textContent = 'X'
-                // isTrue = false 
-            } else {
-                td.textContent = 'O'
-                // isTrue = true
-            }
-            // isTrue = !isTrue
-        })
-        
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this;
     }
-    
+};
 
-    table.appendChild(tr);
-}
-
+ladder.up().up().down().showStep()
