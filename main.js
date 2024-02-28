669dc713194ecef913efb63ba893a980b597a620
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-28 15:26:56 +0200

    hw-23

--- main.js
+++ main.js
@@ -1,43 +1,27 @@
-const todo_wrapper = document.querySelector('#todo_list') 
-const add_todo = document.querySelector('#add_todo')
-const todo_input = document.querySelector('#todo_input') 
-const todo_list = JSON.parse(localStorage.getItem('todo_list')) ? 
-     JSON.parse(localStorage.getItem('todo_list')) : []
-updateTodoList()
+let heightArr = +prompt('Задайте довжину масиву')
 
+if(heightArr > 0) {
+    const newArr = []
 
-function updateTodoList() {
-    todo_wrapper.innerHTML = '';
-    localStorage.setItem('todo_list', JSON.stringify(todo_list))
+for(let i = 0; i < heightArr; i++) {
+    let elemArr = +prompt("Задайте наповнення масиву")
+    newArr.push(elemArr)
     
-    todo_list.forEach((todo) => {
-        const temp_todo = document.createElement('div');
-        temp_todo.textContent = todo.title;
-        temp_todo.classList.add('todo');
-        
-        todo_wrapper.appendChild(temp_todo)
+    }
+console.log(newArr)
 
-        temp_todo.addEventListener('click', () => {
-            console.log(temp_todo.style.textDecoration === 'line-through')
-            if (temp_todo.style.textDecoration === 'line-through') {
-                temp_todo.style.textDecoration = 'none'
-            } else {
-                temp_todo.style.textDecoration = 'line-through'
-                todo.complete === true
-            }
-        })
-    })
+function compareNumbers(a, b) {
+    return a - b;
+    }
+newArr.sort(compareNumbers)
+console.log(newArr)
+
+newArr.splice(1 , 3)
+console.log(newArr)
 }
 
-add_todo.addEventListener('click', () => {
-    if(todo_input.value) {
-        todo_list.push({
-            title: todo_input.value,
-            complete: false,
-        })
-    }
 
-    updateTodoList()
-})
+
+
 
 
